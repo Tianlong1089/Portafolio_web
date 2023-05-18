fcee4648d5b2869e24582cf086b6e1a2af808bae
Tianlon1089 <carloscarrera@ciencias.unam.mx>
2023-05-18 16:12:43 -0600

    Create columns, image, and description , May 18

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+st.header("Home")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("/home/tianlong55/Downloads/PyCharm_Projects/app2_portafolio/images/photo.png")
+
+with col2:
+    st.title("Carlos Carrera")
+    content = """
+    Hi, I am Carlos! I am a physicist, data scientist, programmer and teacher at UNAM. I graduated in 2023.
+     I  worked as undergraduate researcher by ICN, UNAM developing algorithms and software solutions for optimazing 
+     science experiments.  Now, I have moved in order to reach new areas out of the scientific researching and I am focused in 
+     continue my job career as a full time programmer. 
+      """
+    st.info(content)
