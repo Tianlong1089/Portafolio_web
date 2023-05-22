abb09408b53b95c970d36f9bb8aa1e0992a2cde4
Tianlon1089 <carloscarrera@ciencias.unam.mx>
2023-05-22 13:55:42 -0600

    Render titles in to columns, May 18

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
@@ -23,3 +24,14 @@ content_1="""
         Below you can find some of the apps I have built in Python, C and other stuff. Feel free to contact me !!
         """
 st.text(content_1)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("/home/tianlong55/Downloads/PyCharm_Projects/app2_portafolio/data.csv",sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
